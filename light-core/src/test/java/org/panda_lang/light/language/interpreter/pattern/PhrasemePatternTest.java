1d57d9638c09b349a91595de6b7c41f885e7b3c8
dzikoysk <dzikoysk@dzikoysk.net>
2018-07-19 00:21:05 +0200

    Start developing the Phraseme system by TDD

---
+++ light-core/src/test/java/org/panda_lang/light/language/interpreter/pattern/PhrasemePatternTest.java
@@ -0,0 +1,13 @@
+package org.panda_lang.light.language.interpreter.pattern;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PhrasemePatternTest {
+
+    @Test
+    public void testPhrasemePattern() {
+        Assertions.fail("Not Implemented");
+    }
+
+}
